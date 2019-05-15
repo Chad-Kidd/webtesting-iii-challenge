005a4894d1911d95984ab99ad3e3ca6df9d3cd56
chad kidd <ckiddwebdev@gmail.com>
2019-05-15 14:30:28 -0700

    Dashboard tests PASSED

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,21 @@
 // Test away
+import React from 'react';
+import rendered from 'react-test-renderer';
+import Dashboard from './Dashboard';
+
+//tests passed
+describe('<Dashboard />', () => {
+  it('runs the tests', () => {
+    expect(true).toBe(true)
+  });
+  // 2. write this test
+  it('matches snapshot', () => {
+    const tree = rendered.create(<Dashboard />); // generates a DOM tree
+
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  
+
+});
