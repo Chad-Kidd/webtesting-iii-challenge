909c18ececd230cfa3ae8a5ae703ea9f4f6f14d8
chad kidd <ckiddwebdev@gmail.com>
2019-05-15 14:53:07 -0700

    controls tests PASSED

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -5,17 +5,15 @@ import Dashboard from './Dashboard';
 
 //tests passed
 describe('<Dashboard />', () => {
+    //runs tests make sure tests are running
   it('runs the tests', () => {
     expect(true).toBe(true)
   });
-  // 2. write this test
+  // 2. tests to make sure this matches what is rendered 
   it('matches snapshot', () => {
     const tree = rendered.create(<Dashboard />); // generates a DOM tree
 
     // snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
   });
-
-  
-
 });
